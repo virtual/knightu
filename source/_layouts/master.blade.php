1910a0c6f7e88813dd49efeda6314dd93d96abf8
Jeanine Schoessler <jeanine@cascadin.com>
2020-01-06 02:41:22 +0000

    fix assets links for prod

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>{{ $page->siteTitle }}</title>
-        <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
+        <link rel="stylesheet" href="{{ $page->baseUrl }}/assets/css/main.css">
     </head>
     <body class="antialiased font-sans">
       @include('_partials/header')
